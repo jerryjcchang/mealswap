d011724f1b8ed34b5c6066ded2971fefaef68723
github username <github email address>
2019-01-02 17:06:07 -0500

    commit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :companies, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:new, :create, :show]
   resources :meals, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
