3c8f54f27c8bb95f34af5ebe8f167c2bf47ec241
github username <github email address>
2019-01-03 12:18:46 -0500

    completed email add, new gem: email_address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_222110) do
+ActiveRecord::Schema.define(version: 2019_01_03_143637) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_01_02_222110) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "email"
   end
 
 end
