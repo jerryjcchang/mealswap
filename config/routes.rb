79dce9297f69a41609083aa56140d02e347b5449
github username <github email address>
2019-01-07 16:39:43 -0500

    added one more comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  #Added note: you may want to add a catchall route that pushes back to your
+  #root page.
 end
